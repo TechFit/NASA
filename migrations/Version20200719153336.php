f430dafb26af9efe3af09fc14c0f3eaf84fc5578
nempak <nempak@gmail.com>
2020-07-19 18:44:00 +0300

    Migration, Entity, Repository

---
+++ migrations/Version20200719153336.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Class Version20200719153336
+ */
+final class Version20200719153336 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Create asteroid table.';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('CREATE TABLE asteroid '
+            .'(id INT AUTO_INCREMENT NOT NULL, date DATETIME NOT NULL, '
+            .'reference CHAR(36) NOT NULL COMMENT \'(DC2Type:guid)\', name VARCHAR(255) NOT NULL,'
+            .' speed DOUBLE PRECISION NOT NULL, is_hazardous TINYINT(1) NOT NULL, PRIMARY KEY(id)) '
+            .'DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB'
+        );
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('DROP TABLE asteroid');
+    }
+}
---
+++ src/Entity/Asteroid.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AsteroidRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=AsteroidRepository::class)
+ */
+class Asteroid
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $date;
+
+    /**
+     * @ORM\Column(type="guid")
+     */
+    private $reference;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $speed;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $is_hazardous;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getReference(): ?string
+    {
+        return $this->reference;
+    }
+
+    public function setReference(string $reference): self
+    {
+        $this->reference = $reference;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getSpeed(): ?float
+    {
+        return $this->speed;
+    }
+
+    public function setSpeed(float $speed): self
+    {
+        $this->speed = $speed;
+
+        return $this;
+    }
+
+    public function getIsHazardous(): ?bool
+    {
+        return $this->is_hazardous;
+    }
+
+    public function setIsHazardous(bool $is_hazardous): self
+    {
+        $this->is_hazardous = $is_hazardous;
+
+        return $this;
+    }
+}
